cef1ff24f029926a1f9c89345d6b29160bce426d
20djm <mostdarryl@gmail.com>
2018-08-13 19:14:56 -0400

    directory structure

--- react_django/Lambda_Map/Lambda_Map/urls.py
+++ react_django/Lambda_Map/Lambda_Map/urls.py
@@ -1,4 +1,4 @@
-"""example URL Configuration
+"""Lambda_Map URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('react/', TemplateView.as_view(template_name='index.html'))
 ]
