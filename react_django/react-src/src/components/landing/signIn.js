b7dea7a90f269ee22abe596c6e5dbb2eb0d7f59f
dresean <williamsdresean@gmail.com>
2018-08-13 19:44:46 -0400

    setting up boiler plates

---
+++ react_django/react-src/src/components/landing/signIn.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class signIn extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div>
+        <div className='container'>
+        <h1> Sign In</h1>
+        <form>
+          <label>
+            Email:
+            <input type="email" name="email" placeholder="Enter your email address"/>
+          </label>
+          <label>
+            Password:
+            <input type="password" name="password" placeholder="Enter your password"
+            />
+          </label>
+          <h1> Or... Sign in with</h1> <button> Google </button>
+          <br/>
+          <br/>
+          <button>Sign In!</button>
+          <button>Cancel</button>
+        </form>
+        </div>
+      </div>
+    )
+  }
+}
--- react_django/react-src/src/index.js
+++ react_django/react-src/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { BroweserRouter as Router } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+// import rootReducer from './reducers/rootReducers/index.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+ReactDOM.render(
+<Provider>
+  <Router>
+    <App />
+  </Router>
+</Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
