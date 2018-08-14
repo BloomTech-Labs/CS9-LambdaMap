7511ba52f1674beb4c801de792cda102d50c68fb
Eric Gomez <eric_gomez_1@hotmail.com>
2018-08-14 12:42:35 -0700

    added a form for user creation

--- react_django/django-src/django-src/urls.py
+++ react_django/django-src/django-src/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django_app.views import register_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('api/register/', register_page)
 ]
