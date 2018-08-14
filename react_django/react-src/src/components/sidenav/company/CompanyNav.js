9c32ff860ef9949b0b2c51c3dbd1d7d082762104
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-14 12:36:58 -0700

    updated company nav

--- react_django/react-src/src/components/sidenav/company/CompanyNav.js
+++ react_django/react-src/src/components/sidenav/company/CompanyNav.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Sidenav.css';
 import maplogored from './maplogored.svg';
 import lambdalogo from './lambdalogo.png';
-import { FaHome, FaAngleRight, FaMap, FaMapMarker, FaStar, FaEnvelope } from 'react-icons/fa';
+import { FaHome, FaCopyright, FaAngleRight, FaMap, FaMapMarker, FaStar, FaEnvelope } from 'react-icons/fa';
 import { MdSettings, MdAttachMoney } from 'react-icons/md';
 
 export default class CompanyNav extends Component {
@@ -23,6 +23,9 @@ export default class CompanyNav extends Component {
             <li><FaEnvelope className="icons" />Messages</li>
             <li><MdSettings className="icons" />Settings</li>
             <li><MdAttachMoney className="icons" />Billing</li>
+            <footer>
+                <h5 className="footer"><FaCopyright className="footerIcon"/>DreamTeam</h5>
+            </footer>
           </div>
           <div className="arrow">
             <FaAngleRight />
