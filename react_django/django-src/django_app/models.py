473568f70ef949292b3a32087d59744b83340ae6
20djm <mostdarryl@gmail.com>
2018-08-15 16:44:08 -0400

    finished all table schema except billing

--- react_django/django-src/django_app/models.py
+++ react_django/django-src/django_app/models.py
@@ -28,24 +28,41 @@ class Students(models.Model):
     remote = models.BooleanField(default=False)
 
 class Employers(models.Model):
-  username = models.CharField(max_length=50, default='', unique=True)
-  email = models.EmailField(unique=True)
-  password = models.CharField(max_length=50, default='')
-  firstName = models.CharField(max_length=50, default='', unique=True)
-  lastName = models.CharField(max_length=50, default='', unique=True)
-  companyName = models.CharField(max_length=50, default='', unique=True)
-  city = models.CharField(max_length=50, default='', unique=True)
-  state = models.CharField(max_length=50, default='', unique=True)
-  currentlySubscribed = models.BooleanField(default=False)
+    username = models.CharField(max_length=50, default='', unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50, default='')
+    firstName = models.CharField(max_length=50, default='', unique=True)
+    lastName = models.CharField(max_length=50, default='', unique=True)
+    companyName = models.CharField(max_length=50, default='', unique=True)
+    city = models.CharField(max_length=50, default='', unique=True)
+    state = models.CharField(max_length=50, default='', unique=True)
+    currentlySubscribed = models.BooleanField(default=False)
 
 class Student_Phones(models.Model):
     userID = models.ForeignKey(Students,on_delete=models.CASCADE)
     number = models.CharField(max_length=20,unique=True)
 
-
 class Employer_Phones(models.Model):
     userID = models.ForeignKey(Employers,on_delete=models.CASCADE)
     number = models.CharField(max_length=20,unique=True)
 
+class Student_Favorites(models.Model):
+    studentID = models.ForeignKey(Students,on_delete=models.CASCADE)
+    employerID = models.ForeignKey(Employers,on_delete=models.CASCADE)
 
+class Employer_Favorites(models.Model):
+    employerID = models.ForeignKey(Employers,on_delete=models.CASCADE)
+    studentID = models.ForeignKey(Students,on_delete=models.CASCADE)
+
+class Messages(models.Model):
+    employerID = models.ForeignKey(Employers,on_delete=models.CASCADE)
+    studentID = models.ForeignKey(Students,on_delete=models.CASCADE)
+    content = models.CharField(max_length=400,unique=True)
+    postTime = models.CharField(max_length=400,unique=True)
+
+class Job_Listings(models.Model):
+    employerID = models.ForeignKey(Employers,on_delete=models.CASCADE)
+    positionTitle = models.CharField(max_length=400,unique=True)
+    post_time = models.CharField(max_length=400,unique=True)
+    remote = models.BooleanField(default=False)
 
