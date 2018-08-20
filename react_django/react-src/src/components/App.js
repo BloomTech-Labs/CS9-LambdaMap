1a4dfef9b8933ddc15df750cc15aba11e5d46826
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-20 09:14:00 -0700

    with mark's changes as well

--- react_django/react-src/src/components/App.js
+++ react_django/react-src/src/components/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import HPsettings from './settings/HP-settings/HPsettings';
 import JSprofile from './jobseeker-profile/JSprofile';
-// import StudentSettings from './HPprofile/hpProfileView'
 import JSlistView from './list-views/jobseekers-HPview/JSlistView';
+import HpProfileView from './components/hpProfileView/hpProfileView';
+import StudentSettings from './components/student/studentSettings/studentSettings';	
+import StudentSettings from './components/settings/studentSettings/studentSettings';
 
 
 class App extends Component {
@@ -15,6 +17,8 @@ class App extends Component {
           <Route exact path="/" component={JSprofile} />
           <Route path="/jobseekers" component={JSlistView} />
           <Route path="/settings" component={HPsettings} />
+          <Route path="/hpprofile"  component={HpProfileView} />
+           <Route path="/studentsettings" component={StudentSettings} />
       </div>
     );
   }
--- react_django/react-src/src/components/HpProfileView/hpProfileView.js
+++ react_django/react-src/src/components/HpProfileView/hpProfileView.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { FaEnvelope, FaCheck, FaLink, FaStar } from 'react-icons/fa'
-import './hpProfileView.css'
+import './hpProfileView.css';
+import JobSeekerNav from '../nav/job-seeker/JobSeekerNav';
 
- export default class StudentSettings extends Component{
+export default class HpProfileView  extends Component{
     render() {
         return(
+            <div>
+            <JobSeekerNav />
             <div className="hp">
             <div className="hp-div">
             <div className="hp-info">
             <div className="hp-title-div">
             <div className="hp-location-div">
             <h1 className="hp-title">Amazon</h1>
-            <h3 className="hp-location">Seatle, WA</h3>
+            <h3 className="hp-location">Seattle, WA</h3>
             </div>
             <img className="hp-photo" src='http://www.vmastoryboard.com/wp-content/uploads/2014/08/Amazon-A-Logo.jpg' alt='amazon logo' />
             </div>
@@ -21,11 +24,10 @@ import './hpProfileView.css'
              consectetur adipiscing elit, sed do eiusmod tempor
              incididunt ut labore et dolore magna aliqua.</p>
              <div className='hp-remote'>
-            <button className="remote-check"><FaCheck /></button>
+            <button className="remote-check"><FaCheck className="check-icon" /></button>
                 <p className="hp-email">Open to remote employees</p>
                 </div>
              </div>
-           
             </div>
              <div className="hp-email-div">
              <FaEnvelope className="mail-logo" /><p className="hp-email">amazon.recruiter@gmail.com</p>
@@ -37,7 +39,7 @@ import './hpProfileView.css'
              <p className="hp-listing-title">Job Listings</p>
              </div>
              <div className='hp-job-cards'>
-            <h5 className="hp-card-title">JR WEB DEVELOPER <FaStar className="link-icon"/></h5>
+            <h5 className="hp-card-title">JR WEB DEVELOPER <FaStar className="star-icon"/></h5>
             <p className="hp-card-text">Lorem ipsum dolor sit amet,
              consectetur adipiscing elit, sed do eiusmod tempor
              incididunt ut labore et dolore magna aliqua.</p>
@@ -46,7 +48,7 @@ import './hpProfileView.css'
              </div>
              </div>
              <div className='hp-job-cards'>
-            <h5 className="hp-card-title">JR WEB DEVELOPER <FaStar className="link-icon"/></h5>
+            <h5 className="hp-card-title">JR WEB DEVELOPER <FaStar className="star-icon"/></h5>
             <p className="hp-card-text">Lorem ipsum dolor sit amet,
              consectetur adipiscing elit, sed do eiusmod tempor
              incididunt ut labore et dolore magna aliqua.</p>
@@ -55,7 +57,7 @@ import './hpProfileView.css'
              </div>
              </div>
              <div className='hp-job-cards'>
-            <h5 className="hp-card-title">JR WEB DEVELOPER <FaStar className="link-icon"/></h5>
+            <h5 className="hp-card-title">JR WEB DEVELOPER <FaStar className="star-icon"/></h5>
             <p className="hp-card-text">Lorem ipsum dolor sit amet,
              consectetur adipiscing elit, sed do eiusmod tempor
              incididunt ut labore et dolore magna aliqua.</p>
@@ -64,7 +66,7 @@ import './hpProfileView.css'
              </div>
              </div>
              <div className='hp-job-cards'>
-            <h5 className="hp-card-title">JR WEB DEVELOPER <FaStar className="link-icon"/></h5>
+            <h5 className="hp-card-title">JR WEB DEVELOPER <FaStar className="star-icon"/></h5>
             <p className="hp-card-text">Lorem ipsum dolor sit amet,
              consectetur adipiscing elit, sed do eiusmod tempor
              incididunt ut labore et dolore magna aliqua.</p>
@@ -74,9 +76,12 @@ import './hpProfileView.css'
              </div>
              </div>
              <div className="hp-header">
-            <button className="button">Logout</button>
+             <div className="signout">
+          <button className="signoutbutton">Sign Out</button>
+        </div>
             </div>
+             </div>
              </div>
         )
     }
-} 
+}
