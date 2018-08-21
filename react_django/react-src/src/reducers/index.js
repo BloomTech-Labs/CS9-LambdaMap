bdcd40a0a1c1d96091a015049f7cc977872979d7
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-21 13:06:44 -0700

    getClients redux working, getHP still working on...the styling of the list view of clients is messed up but that's ok I planned on changing it anyway

--- react_django/react-src/src/reducers/index.js
+++ react_django/react-src/src/reducers/index.js
@@ -1,44 +1,59 @@
 // still in progress
 
-import { FETCH_USERS, FETCHED_USERS, ERROR_FETCHING } from '../actions';
+import { FETCH_HP, FETCHED_HP, FETCH_CLIENTS, FETCHED_CLIENTS, ERROR_FETCHING } from '../actions';
 
  const initialState = {
-   users: [],
-   fetchingUsers: false,
+   clients: [],
+   fetchingClients: false,
    error: null
+  //  hiring_partner: [],
+  //  fetchingHPs: false,
   //  user: {},
  }
 
 export const rootReducer = (state = initialState, action) => {
 
   switch(action.type) {
+    
+    case FETCH_CLIENTS:
+      return Object.assign({}, state, { 
+        fetchingClients: true 
+    });
 
-      case FETCH_USERS:
-        return Object.assign({}, state, { 
-          fetchingUsers: true 
-      });
+    case FETCHED_CLIENTS:
+      console.log(...action.payload)      
+      return Object.assign({}, state, {
+        clients: [ ...action.payload ],
+        fetchingClients: false,
+        error: null
+    });
 
-      case FETCHED_USERS:
-      console.log(action.payload)      
-        return Object.assign({}, state, {
-          users: [ ...action.payload.users ],
-          fetchingUsers: false,
-          error: null
-      });
+    // case FETCH_HP:
+    //   return Object.assign({}, state, { 
+    //     fetchingHPs: true 
+    // });
+
+    // case FETCHED_HP:
+    // console.log(action.payload)      
+    //   return Object.assign({}, state, {
+    //     hiring_partner: [ ...action.payload.hiring_partner ],
+    //     fetchingHPs: false,
+    //     error: null
+    // });
       
-      // case FETCH_USER:
-      //   return Object.assign({}, state, {
-      //     user: action.payload , 
-      //     fetchuser: true });
+    // case FETCH_USER:
+    //   return Object.assign({}, state, {
+    //     user: action.payload , 
+    //     fetchuser: true });
 
-      case ERROR_FETCHING:
-        return Object.assign({}, state, {
-          error: action.payload,
-      });
+    case ERROR_FETCHING:
+      return Object.assign({}, state, {
+        error: action.payload,
+    });
 
-      default:
-        return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
