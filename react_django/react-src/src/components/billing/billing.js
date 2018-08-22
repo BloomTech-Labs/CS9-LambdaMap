48d4b363d9cfb4934cb119bc8ea9a07395559d93
Your Name <you@example.com>
2018-08-22 13:43:16 -0500

    style changes to HPProfile / stripe dependency added / billing page added

---
+++ react_django/react-src/src/components/billing/billing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HPNav from '../nav/company/HPnav';
+
+export default class Billing extends Component{
+    render() {
+        return(
+            <div>
+            <HPNav />
+            
+            </div>
+        )
+    }
+}
