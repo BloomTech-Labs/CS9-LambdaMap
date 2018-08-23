9d90f09d411ddd2bb930b9fcee265be8b5fdc51b
Your Name <you@example.com>
2018-08-23 13:14:14 -0500

    minor style edit and billing changes

--- react_django/react-src/src/components/billing/billing.js
+++ react_django/react-src/src/components/billing/billing.js
@@ -6,13 +6,22 @@ import './billing.css';
 class Billing extends Component{
     constructor(props) {
         super(props);
+        this.state = {complete: false};
         this.submit = this.submit.bind(this);
       }
     
-      async submit(ev) {
-        // User clicked submit
-      }
+      async submit(e) {
+        // let {token} = await this.props.stripe.createToken({name: "Name"});
+        let response = await fetch("/charge", {
+          method: "POST",
+          headers: {"Content-Type": "text/plain"},
+        //   body: token.id
+        });
+      
+        if (response.ok) console.log("Purchase Complete!")
+    }
     render() {
+        if (this.state.complete) return <h1>Purchase Complete</h1>;
         return(
         <div>
             <HPNav />
