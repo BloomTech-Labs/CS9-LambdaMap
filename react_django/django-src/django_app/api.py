64c60ec89c43325bc989b49bb5b20296ef199d2e
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-24 10:02:42 -0700

    added import re

--- react_django/django-src/django_app/api.py
+++ react_django/django-src/django_app/api.py
@@ -5,7 +5,7 @@ from .security import encrypt_password, verify_password
 from .authentication import create_token, verify_token
 from django.db import IntegrityError
 from django.core import serializers
-import json
+import json, re
 
 def str_to_bool(str):
     return str[0] == 'T' or str[0] == 't'
