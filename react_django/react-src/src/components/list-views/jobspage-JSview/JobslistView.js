0f178bfd2e3453816df30e7ec5a2e6e845608420
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-24 06:05:45 -0700

    very minor style changes just for mvp and visibility

--- react_django/react-src/src/components/list-views/jobspage-JSview/JobslistView.js
+++ react_django/react-src/src/components/list-views/jobspage-JSview/JobslistView.js
@@ -1,113 +1,138 @@
 // Hiring Partner View for list of job seekers with filter
 
-import React, { Component } from 'react';
-import './JobslistView.css';
-import { FaCaretDown, FaStar, FaSquare, FaTwitter, FaLinkedin, FaGithub, FaCodepen, FaFile, FaDesktop, FaEnvelope, FaPhoneSquare } from 'react-icons/fa';
-import HPnav from '../../nav/company/HPnav';
+import React, { Component } from "react";
+import "./JobslistView.css";
+import { FaCaretDown, FaStar } from "react-icons/fa";
+import { GoClock } from "react-icons/go";
+import HPnav from "../../nav/company/HPnav";
 import defaultuser from "./defaultuser.svg";
-import { Link } from 'react-router-dom';
-import { get_listings } from '../../../actions';
-import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
+import { get_listings } from "../../../actions";
+import { connect } from "react-redux";
+
+const Timestamp = require("react-timestamp");
 
 class JobslistView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jobListing: [],
-    }
+      jobListing: []
+    };
   }
 
   componentDidMount = () => {
-    console.log(this.props.get_listings)
     this.props.get_listings();
-  }
+  };
 
   render() {
-    console.log(this.props)
-    console.log(this.state.job_listings)
     return (
       <div className="main-jobslist">
         <HPnav />
         <div className="signout">
-        <div className="backgroundskew"></div>
-        <div className="backgroundskew2"></div>
+          <div className="backgroundskew" />
+          <div className="backgroundskew2" />
           <button className="signoutbutton">Sign Out</button>
         </div>
         <div className="jobslist-container">
           <div className="filter-main">
             <header className="header">Job Listings</header>
-            <div className="filter-display" >
-            <input type="checkbox" />
-            <button>Filter</button><FaCaretDown className="filter-icon" />
+            <div className="filter-display">
+              <input type="checkbox" />
+              <button>Filter</button>
+              <FaCaretDown className="filter-icon" />
               <div className="filter-options">
                 <label className="switch">
-                  <input type="checkbox"></input>
-                  <span className="slider round"></span>
-                </label><h4>Software Engineer</h4>
-              
-                <label className="switch">
-                  <input type="checkbox"></input>
-                  <span className="slider round"></span>
-                </label><h4>Full Stack Developer</h4>
-              
-                <label className="switch">
-                  <input type="checkbox"></input>
-                  <span className="slider round"></span>
-                </label><h4>Front-end Developer</h4>
-              
-                <label className="switch">
-                  <input type="checkbox"></input>
-                  <span className="slider round"></span>
-                </label><h4>Back-end Developer</h4>
+                  <input type="checkbox" />
+                  <span className="slider round" />
+                </label>
+                <h4>Software Engineer</h4>
 
                 <label className="switch">
-                  <input type="checkbox"></input>
-                  <span className="slider round"></span>
-                </label><h4>UI/UX Developer</h4>
-              
+                  <input type="checkbox" />
+                  <span className="slider round" />
+                </label>
+                <h4>Full Stack Developer</h4>
+
                 <label className="switch">
-                  <input type="checkbox"></input>
-                  <span className="slider round"></span>
-                </label><h4>iOS Developer</h4>
-              
+                  <input type="checkbox" />
+                  <span className="slider round" />
+                </label>
+                <h4>Front-end Developer</h4>
+
                 <label className="switch">
-                  <input type="checkbox"></input>
-                  <span className="slider round"></span>
-                </label><h4>Android Developer</h4>
+                  <input type="checkbox" />
+                  <span className="slider round" />
+                </label>
+                <h4>Back-end Developer</h4>
+
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider round" />
+                </label>
+                <h4>UI/UX Developer</h4>
+
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider round" />
+                </label>
+                <h4>iOS Developer</h4>
+
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider round" />
+                </label>
+                <h4>Android Developer</h4>
               </div>
             </div>
           </div>
-          <hr className="cards-hr"></hr>
+          <hr className="cards-hr" />
           <div className="joblistingcards-container">
-          <div className="cards-main">
-            {this.props.jobListing.job_listings.map(job_listing =>
+            <div className="cards-main">
+              {this.props.jobListing.job_listings.map(job_listing => (
                 <div key={job_listing.ID} className="joblisting-cards">
-                  <Link to={'/'} className="profile-link">
+                  <Link
+                    to={`/hpprofile/${job_listing.ID}`}
+                    className="profile-link"
+                  >
                     <div className="card-header">
-                      <img src={defaultuser} className="Joblistcardpic" alt="Job Seeker" />
-                        <div className="jobscard-name">
-                          <h3>{job_listing.company_name}</h3>
-                          <h5>{job_listing.city}{job_listing.state}</h5>
-                        </div>
-                          <FaStar className="card-favIcon"/>
+                      <img
+                        src={defaultuser}
+                        className="Joblistcardpic"
+                        alt="Job Seeker"
+                      />
+                      <div className="jobscard-name">
+                        <h3>{job_listing.company_name}</h3>
+                        <h5>
+                          {job_listing.city}
+                          {job_listing.state}
+                        </h5>
+                      </div>
+                      <FaStar className="card-favIcon" />
                     </div>
-                    {job_listing.jobListings.map(jls =>
+                    <hr />
+                    {job_listing.jobListings.map(jls => (
                       <div className="card-info">
+                        <p>
+                          <GoClock className="clock" />
+                          <Timestamp time={jls.posted_time} actualSeconds />
+                        </p>
                         <p className="card-bio">{jls.job_title}</p>
                         <p className="card-bio">{jls.job_desc}</p>
-                      </div>)}
+                        <hr />
+                      </div>
+                    ))}
                   </Link>
-                </div>)}
-              </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
-    )
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state)
   return {
     jobListing: state.jobListing,
     fetchingListings: state.fetchingListings,
@@ -115,4 +140,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { get_listings })(JobslistView);
+export default connect( mapStateToProps, { get_listings })(JobslistView);
