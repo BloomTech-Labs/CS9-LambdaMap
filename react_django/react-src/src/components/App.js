9a6f1e48b886c01f7176a11a17d5b7fdd1cbb52a
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-23 18:02:16 -0700

    app wasn't saved

--- react_django/react-src/src/components/App.js
+++ react_django/react-src/src/components/App.js
@@ -2,29 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import HPsettings from './settings/HP-settings/HPsettings';
-import JSprofile from './jobseeker-profile/JSprofile';
 import JSlistView from './list-views/jobseekers-HPview/JSlistView';
 import HpProfileView from './HpProfileView/hpProfileView';
-import StudentSettings from './settings/studentSettings/studentSettings';	
-import MapView from './map/map';
+// import MapView from './map/map';
 import Billing from './billing/billing';
 import compass from './compass.png'
 import { Link } from 'react-router-dom';
-import hiringPartnerFavorites from './favorites/hiringPartnerFavorites/hiringPartnerFavorites';	
+import JobslistView from './list-views/jobspage-JSview/JobslistView';
+import HPFavorites from './favorites/hiringPartnerFavorites/HPFavorites';
 
 class App extends Component {
  
   render() {
     return (
       <div className="App">
-          <Route exact path="/" component={JSprofile} />
+          <Route exact path="/" component={JobslistView} />
           <Route path="/jobseekers" component={JSlistView} />
           <Route path="/billing" component={Billing} />
           <Route path="/settings" component={HPsettings} />
           <Route path="/hpprofile"  component={HpProfileView} />
-          <Route path="/studentsettings" component={StudentSettings} />
-          <Route path="/map" component={MapView} />
-          <Route path="/hiringPartnerFavorites" component={hiringPartnerFavorites} />
+          {/* <Route path="/map" component={MapView} /> */}
+          <Route path="/favorites/" component={HPFavorites} />
           <Link to='/map'><img src={compass} alt='compass' className='compass' /> </Link>
       </div>
     );
