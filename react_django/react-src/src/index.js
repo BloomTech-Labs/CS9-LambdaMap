49bab8a7b2b440a0c2b1e9bd3c7aa404fd745184
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-08-23 18:27:27 -0700

    troubleshooting

--- react_django/react-src/src/index.js
+++ react_django/react-src/src/index.js
@@ -8,13 +8,18 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+import {Elements, StripeProvider} from 'react-stripe-elements';
 
 const store = createStore( reducers, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
     <Provider store={store}>
     <Router>
+    <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+    <Elements>
         <App />
+        </Elements>
+        </StripeProvider>
     </Router>
     </Provider>, 
 document.getElementById('root'));
